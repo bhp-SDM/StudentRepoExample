64a920c07209e98c48d96408af218462675b1190
Bent H. Pedersen <bhp@easv.dk>
2022-11-06 23:11:18 +0100

    CreateStudentServce (#1)
    Co-authored-by: BentHPedersen <benthpedersen@mail.com>

---
+++ StudentRepoExample/Core/Service/StudentService.cs
@@ -0,0 +1,14 @@
+﻿using StudentRepoExample.Core.Interfaces;
+
+namespace StudentRepoExample.Core.Service
+{
+    public class StudentService
+    {
+        private IStudentRepository _studentRepository;
+
+        public StudentService(IStudentRepository repo)
+        {
+            _studentRepository = repo ?? throw new ArgumentNullException();
+        }
+    }
+}
