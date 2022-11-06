1adb4aef410f62ade473f20141aeafd5ad916305
BentHPedersen <benthpedersen@mail.com>
2022-11-06 23:00:46 +0100

    CreateStudentServce

---
+++ StudentRepoExample/Core/Interfaces/IStudentRepository.cs
@@ -0,0 +1,18 @@
+﻿using StudentRepoExample.Core.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentRepoExample.Core.Interfaces
+{
+    public interface IStudentRepository
+    {
+        void Add(Student s);
+        void Update(Student s);
+        void Delete(Student s);
+        Student Get(int id);
+        IEnumerable<Student> GetAll();
+    }
+}
---
+++ XunitTestProject/StudentServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using StudentRepoExample.Core.Interfaces;
+using StudentRepoExample.Core.Service;
+using Xunit;
+
+namespace XunitTestProject
+{
+    public class StudentServiceTest
+    {
+        [Fact]  
+        public void CreateStudentService_ValidStudentRepository_Test()
+        {
+            // Arrange
+            Mock<IStudentRepository> repoMock = new Mock<IStudentRepository>();
+
+            // Act
+            var service = new StudentService(repoMock.Object);
+
+            // Assert
+            Assert.NotNull(service);
+            Assert.True(service is StudentService);
+        }
+
+        [Fact]
+        public void CreateStudentService_StudentRepositoryIsNull_ExpectArgumentNullException_Test()
+        {
+            // Arrange
+            StudentService? service = null;
+
+            // Act and Assert
+            Assert.Throws<ArgumentNullException>(() => service = new StudentService(null));
+        }
+    }
+}
