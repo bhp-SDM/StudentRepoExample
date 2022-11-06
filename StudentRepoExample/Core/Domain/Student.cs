016bb40ce2b7e23564c099d5fd88e055e7f51592
bhpeasv1 <44821356+bhpeasv1@users.noreply.github.com>
2022-11-06 23:47:51 +0100

    AddStudent (#2)

--- StudentRepoExample/Core/Domain/Student.cs
+++ StudentRepoExample/Core/Domain/Student.cs
@@ -1,15 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace StudentRepoExample.Core.Domain
+﻿namespace StudentRepoExample.Core.Domain
 {
     public class Student
     {
+ 
         public int Id { get; set; }
         public string Name { get; set; }
         public string? Email { get; set; }
+        
+        public Student(int id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+        }
+
+        public Student(int id, string name) : this(id, name, null) { }
     }
 }
